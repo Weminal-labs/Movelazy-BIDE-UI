bc13de02911457aa5113bffa2dd4e9d1b7c4dd54
menh <dinhthienmenh1505@gmail.com>
2025-01-12 12:33:26 +0700

    add file type

---
+++ types/file.type.ts
@@ -0,0 +1,9 @@
+export type File = {
+    id: string;
+    name: string;
+    content: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type FileFolder = 'contracts' | 'scripts' | 'tests';
